df8c44a82529f92f331ccd5146dd7220ba1e268e
Sandhya <134464080+sr2498@users.noreply.github.com>
2026-08-03 08:56:51 -0400

    Clean up BusinessIntelligenceDashboardApplication.java
    Removed unnecessary blank lines in the main application file.

--- src/main/java/com/example/bidashboard/BusinessIntelligenceDashboardApplication.java
+++ src/main/java/com/example/bidashboard/BusinessIntelligenceDashboardApplication.java
@@ -1,9 +1,7 @@
 package com.example.bidashboard;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class BusinessIntelligenceDashboardApplication {
